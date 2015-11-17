80b905eb553ad30ade5d413b7426bac61a1fe6e0
MaVuong <lekhacson@Sons-Mac-mini.local>
2015-11-17 15:15:27 +0700

    ss
    asdasdas

--- src/ScriptDemo.lua
+++ src/ScriptDemo.lua
@@ -5,11 +5,11 @@ function AnimationParse(strinfo)
   local scrt={};
   
   if xpos>240 then
-    scrt["cid"]="1";
+    scrt["cid"]="2";
     scrt["script"]='["RotateBy(2||360)"]';
   else
-    scrt["cid"]="2";
-    scrt["script"]='["JumpAction(4||{500,90})"]';
+    scrt["cid"]="1";
+    scrt["script"]='["JumpAction(4||{420,90})"]';
   end
   
   return encode(scrt);

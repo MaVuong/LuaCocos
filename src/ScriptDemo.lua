9ccb36adbbf6877fdbbc9047b1edcf32613b0c26
MaVuong <dethichthien@me.com>
2015-11-17 14:17:29 +0700

    cmx sa
    lua commit

--- src/ScriptDemo.lua
+++ src/ScriptDemo.lua
@@ -5,10 +5,10 @@ function AnimationParse(strinfo)
   local scrt={};
   
   if xpos>240 then
-    scrt["cid"]="3";
+    scrt["cid"]="2";
     scrt["script"]='["MoveTo(2||{400,320})","RotateBy(2||360)","MoveTo(1||{100,120})"]';
   else
-    scrt["cid"]="2";
+    scrt["cid"]="1";
     scrt["script"]='["MoveTo(2||{400,320})","ChangeColor(2||1||0||0)","MoveTo(1||{200,120})"]';
   end
   

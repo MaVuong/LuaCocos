de657cbe31829e344494daf171da6b4f3fc639e2
MaVuong <lekhacson@Sons-Mac-mini.local>
2015-11-17 14:56:32 +0700

    update new script load
    update new script load

--- src/LuaAnimation.lua
+++ src/LuaAnimation.lua
@@ -44,6 +44,24 @@ function simpleAnimationtapScale(strinfo)
 end
 
 
+function simpleAnimationtapJumpAndMoveAndScale(strinfo)
+   local tbdecode=decode(strinfo);
+  
+  local xpos=tbdecode["tx"]+0;
+  local scrt={};
+  
+  if xpos>240 then
+    scrt["cid"]="2";
+    scrt["script"]='["ScaleTo(1||1.2)","ScaleTo(1||1)",]';
+  else
+    scrt["cid"]="1";
+    scrt["script"]='["JumpAction(4||{500,90})","MoveTo(2||{100,90})",]';
+  end
+  
+  return encode(scrt);
+end
+
+
 
 function AnimationParse(strinfo)
   local tbdecode=decode(strinfo);

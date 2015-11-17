1e941e57e6d5b58e55ca04d87467c1851e2bb590
MaVuong <dethichthien@me.com>
2015-11-17 22:38:01 +0700

    ccsa
    sss

--- src/ScriptDemo.lua
+++ src/ScriptDemo.lua
@@ -5,12 +5,12 @@ function AnimationParse(strinfo)
   local xpos=tbdecode["tx"]+0;
   local scrt={};
   
-  if xpos>240 then
+  if xpos<240 then
     scrt["cid"]="1";
     scrt["script"]='["RotateBy(2||360)"]';
   else
     scrt["cid"]="2";
-    scrt["script"]='["JumpAction(4||{500,90})"]';
+    scrt["script"]='["JumpAction(4||{420,90})"]';
   end
   
   return encode(scrt);

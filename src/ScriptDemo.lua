ac73f625be4d42cb523d5335ebb43db1a6e83564
MaVuong <dethichthien@me.com>
2015-11-17 15:44:10 +0700

    sss
    xxxsad asdas

--- src/ScriptDemo.lua
+++ src/ScriptDemo.lua
@@ -1,15 +1,15 @@
 function AnimationParse(strinfo)
-   local tbdecode=decode(strinfo);
+  local tbdecode=decode(strinfo);
   
   local xpos=tbdecode["tx"]+0;
   local scrt={};
   
-  if xpos<240 then
-    scrt["cid"]="1";
-    scrt["script"]='["RotateBy(2||360)"]';
-  else
+  if xpos>240 then
     scrt["cid"]="2";
-    scrt["script"]='["JumpAction(4||{420,90})"]';
+    scrt["script"]='["ScaleTo(1||1.2)","ScaleTo(1||1)",]';
+  else
+    scrt["cid"]="1";
+    scrt["script"]='["ScaleTo(1||1.2)","ScaleTo(1||1)",]';
   end
   
   return encode(scrt);

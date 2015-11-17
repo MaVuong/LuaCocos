bfcef6b89c32d6c698379a27078fa132d8db8958
MaVuong <dethichthien@me.com>
2015-11-17 22:36:24 +0700

    animation1
    aa1

--- src/ScriptDemo.lua
+++ src/ScriptDemo.lua
@@ -1,6 +1,18 @@
 
 function AnimationParse(strinfo)
-
+  local tbdecode=decode(strinfo);
   
-  return "";
+  local xpos=tbdecode["tx"]+0;
+  local scrt={};
+  
+  if xpos>240 then
+    scrt["cid"]="1";
+    scrt["script"]='["RotateBy(2||360)"]';
+  else
+    scrt["cid"]="2";
+    scrt["script"]='["JumpAction(4||{500,90})"]';
+  end
+  
+  return encode(scrt);
+  
 end

421dce48d78319138e33f96ef35a4bb987233e2d
MaVuong <lekhacson@Sons-Mac-mini.local>
2015-11-17 14:40:01 +0700

    edit update
    edit update

--- src/ScriptDemo.lua
+++ src/ScriptDemo.lua
@@ -1,18 +1,17 @@
 function AnimationParse(strinfo)
-  local tbdecode=decode(strinfo);
+   local tbdecode=decode(strinfo);
   
   local xpos=tbdecode["tx"]+0;
+  print("vi tri x touch la : "..xpos);
   local scrt={};
   
   if xpos>240 then
-    scrt["cid"]="3";
-    scrt["script"]='["MoveTo(2||{400,320})","RotateBy(2||360)","MoveTo(1||{100,120})"]';
-  else
     scrt["cid"]="2";
-    scrt["script"]='["MoveTo(2||{400,320})","ChangeColor(2||1||0||0)","MoveTo(1||{200,120})"]';
+    scrt["script"]='["ChangeColor(2||1||0||0)","ChangeColor(1||1||1||1)",]';
+  else
+    scrt["cid"]="1";
+    scrt["script"]='["ChangeColor(2||1||0||0)","ChangeColor(1||1||1||1)",]';
   end
-  
-  
   
   return encode(scrt);
 end

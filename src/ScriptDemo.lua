02cc4e491ef933c52022cc6d65c5fe322014befb
MaVuong <dethichthien@me.com>
2015-11-17 22:39:44 +0700

    animation 2222
    aaaa xxx2

--- src/ScriptDemo.lua
+++ src/ScriptDemo.lua
@@ -5,12 +5,12 @@ function AnimationParse(strinfo)
   local xpos=tbdecode["tx"]+0;
   local scrt={};
   
-  if xpos<240 then
-    scrt["cid"]="1";
-    scrt["script"]='["RotateBy(2||360)"]';
-  else
+  if xpos>240 then
     scrt["cid"]="2";
-    scrt["script"]='["JumpAction(4||{420,90})"]';
+    scrt["script"]='["ScaleTo(1||1.2)","ScaleTo(1||1)",]';
+  else
+    scrt["cid"]="1";
+    scrt["script"]='["JumpAction(4||{500,90})","MoveTo(2||{100,90})",]';
   end
   
   return encode(scrt);
